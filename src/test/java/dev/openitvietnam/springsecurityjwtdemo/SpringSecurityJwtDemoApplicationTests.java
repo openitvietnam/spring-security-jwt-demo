3ff024328f54b1ebd7066ea790a4936a423bea14
tonghoangvu <tonghoangvu@outlook.com>
2022-11-16 10:29:38 +0700

    Init project

---
+++ src/test/java/dev/openitvietnam/springsecurityjwtdemo/SpringSecurityJwtDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.openitvietnam.springsecurityjwtdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
